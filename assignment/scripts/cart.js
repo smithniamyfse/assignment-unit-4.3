4a7ab78aea985025d7023ea469fad2e9ff44e8bb
Amy Smith <smithniamyfse@gmail.com>
2023-05-28 17:29:32 -0500

    Created basket var and set to empty array; created addItem function with the requirements; created listItems function with the required actions.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,16 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item){
+    basket.push(item);
+    return true;
+}
 
+function listItems(){
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }
+}
 
+console.log(`Basket is ${basket}`);
+console.log('Adding Cheese (expect true)', addItem(' ' + 'Cheese'));
+console.log(`Basket is ${basket}`);
+console.log('Adding Strawberries (expect true)', addItem(' ' + 'Strawberries'));
+console.log(`Basket is ${basket}`);
+console.log('Adding Mountain Dew Code Red (expect true)', addItem(' ' + 'Mountain Dew Code Red'));
+console.log(`Basket is ${basket}`);
+console.log('Adding Spinach (expect true)', addItem(' ' + 'Spinach'));
+console.log(`Basket is ${basket}`);
+console.log('Adding Coffee (expect true)', addItem(' ' + 'Coffee'));
+console.log(`Basket is ${basket}`);
 
-
-
-
-
-
-
+listItems();
 
 
 // DO NOT MODIFY
